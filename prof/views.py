7bc21caa1feb09bbac0c9c467c74d8e801a639a0
jencelis <celislj@clarkson.edu>
2024-04-16 06:08:18 -0400

    Database upload

--- prof/views.py
+++ prof/views.py
@@ -1,5 +1,14 @@
+import django.db.models.signals
 from django.shortcuts import render, HttpResponse
-
+from .models import Instructor
 # Create your views here.
+
+'''
 def home(request):
     return HttpResponse("Professor page")
+'''
+
+
+def home(request):
+    profs = Instructor.objects.all()
+    return render(request, 'prof/index.html',{'Professors':profs})
--- ums/settings.py
+++ ums/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import mysql.connector
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,8 +79,12 @@ WSGI_APPLICATION = 'ums.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'university',
+        'USER': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': 3306,
+        'PASSWORD': 'mypassword'
     }
 }
 
